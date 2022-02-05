8e0ea0e6712b78c0c76a8c0049955f86fecd0953
Epic91 <hernandeze791@gmail.com>
2022-02-05 12:57:45 -0600

    created constant variables needed

---
+++ calculator/script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const ACButton = document.querySelector('[data-all-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+const equalButton = document.querySelector('[data-equals]');
+const previousOperand = document.querySelector('[data-previous-operand]');
+const currentOperand = document.querySelector('[data-current-operand]');
