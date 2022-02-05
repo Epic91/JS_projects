272499b9b2544a2c5b1ee2aab3e16f20c87ff182
Epic91 <hernandeze791@gmail.com>
2022-02-05 13:39:22 -0600

    added a calculator class with methods i will be needing

--- calculator/script.js
+++ calculator/script.js
@@ -1,7 +1,43 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear()
+    }
+
+    clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+        this.currentOperand = number
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand
+
+    }
+}
+
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const ACButton = document.querySelector('[data-all-clear]');
 const deleteButton = document.querySelector('[data-delete]');
 const equalButton = document.querySelector('[data-equals]');
-const previousOperand = document.querySelector('[data-previous-operand]');
-const currentOperand = document.querySelector('[data-current-operand]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
