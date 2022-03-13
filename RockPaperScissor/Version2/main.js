1ace8d320f3f89c96a48c49faeb91da23d5b57eb
Epic91 <hernandeze791@gmail.com>
2022-03-13 03:06:29 -0500

    version2 completed

---
+++ RockPaperScissor/Version2/main.js
@@ -0,0 +1,35 @@
+const choicesDisplay = document.querySelector('#choices')
+const resultDisplay = document.querySelector('#result')
+const choices = ['rock', 'paper', 'scissors']
+
+// Calls on the getResults method and displays what the result is based on the choice
+const handleClick = (e) => {
+    getResults(e.target.innerHTML, choices[Math.floor(Math.random() * choices.length)])
+}
+
+choices.forEach(choice => {
+    const button = document.createElement('button')
+    button.innerHTML = choice
+    button.addEventListener('click', handleClick)
+    choicesDisplay.appendChild(button)
+})
+
+const getResults = (userChoice, computerChoice) => {
+    switch (userChoice + computerChoice) {
+      case 'scissorspaper':
+      case 'rockscissors':
+      case 'paperrock':
+        resultDisplay.innerHTML = 'You chose ' + userChoice + ' and the computer chose ' + computerChoice + ' , YOU WIN!'
+        break
+      case 'paperscissors':
+      case 'scissorsrock':
+      case 'rockpaper':
+        resultDisplay.innerHTML = 'You chose ' + userChoice + ' and the computer chose ' + computerChoice + ' , YOU LOSE!'
+        break
+      case 'scissorsscissors':
+      case 'rockrock':
+      case 'paperpaper':
+        resultDisplay.innerHTML = 'You chose ' + userChoice + ' and the computer chose ' + computerChoice + ' , ITS A DRAW!'
+        break
+    }
+  }
