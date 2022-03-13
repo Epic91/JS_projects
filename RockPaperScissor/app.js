fa957c26f31770137d3846d45db5c2dc314d8f31
Epic91 <hernandeze791@gmail.com>
2022-03-13 01:17:15 -0600

    finished game

--- RockPaperScissor/app.js
+++ RockPaperScissor/app.js
@@ -1,12 +1,59 @@
-const computerChoice = document.getElementById('computer-choice')
+const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
-const results = document.getElementById('results')
+const resultDisplay = document.getElementById('results')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
+let computerChoice
+let result
 
 
 // Displays what the user has chosen using an event listener
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+    getResult()
 }))
+
+// Function, generates a random number and display choice based on the number
+const generateComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length)
+
+    if(randomNumber === 0) {
+        computerChoice = 'rock'
+    }
+    if(randomNumber === 1) {
+        computerChoice = 'paper'
+    }
+    if(randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+const getResult = () => {
+    if (computerChoice === userChoice) {
+        result = "It's a draw!"
+    }
+    if (computerChoice === 'rock' && userChoice === 'paper') {
+        result = "You win!"
+    }
+    if (computerChoice === ' rock' && userChoice === 'scissors') {
+        result = "You lose!"
+    }
+    if (computerChoice === 'paper' && userChoice === 'scissors') {
+        result = "You win!"
+    }
+    if (computerChoice === 'paper' && userChoice === 'rock') {
+        result = "You lose!"
+    }
+    if (computerChoice === 'scissors' && userChoice === 'rock') {
+        result = "You win!"
+    }
+    if (computerChoice === 'scissors' && userChoice === 'paper') {
+        result = "You lose!"
+    }
+    
+    resultDisplay.innerHTML = result
+}
