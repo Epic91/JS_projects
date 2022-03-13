e96dcfe30cb3751afbb85412252ef844aa10ab3b
Epic91 <hernandeze791@gmail.com>
2022-03-13 00:43:58 -0600

    displays what the user has chosen

--- RockPaperScissor/app.js
+++ RockPaperScissor/app.js
@@ -1,3 +1,12 @@
 const computerChoice = document.getElementById('computer-choice')
-const userChoice = document.getElementById('user-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
 const results = document.getElementById('results')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+
+
+// Displays what the user has chosen using an event listener
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+}))
